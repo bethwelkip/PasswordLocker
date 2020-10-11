0e31fdeccc5848e505c007fb263185dfce25631b
Bethwelkip <bkiplimo@princeton.edu>
2020-10-11 19:29:19 -0400

    add test methods

--- user_test.py
+++ user_test.py
@@ -1,3 +1,140 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 import unittest
+
+class ContactTest(unittest.TestCase):
+    '''
+       defines test cases for the user class behaviors
+       Args:
+           unittest.TestCase: TestCase class that helps in creating test cases
+
+     '''
+    def setUp(self):
+        '''
+
+
+        :return:
+        '''
+        pass
+
+    def tearDown(self):
+        '''
+
+
+        :return:
+        '''
+        pass
+
+    def test_init(self):
+        '''
+
+
+         :return:
+        '''
+        pass
+
+    def test_save_credentials(self):
+        '''
+
+
+        :return:
+        '''
+        pass
+
+    def test_display_creds(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_delete_credential(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_create_credential(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_add_credential(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_create_user(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_login(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    def test_save_user(self):
+        '''
+
+
+        :return:
+
+        '''
+        pass
+
+    #############################################################################
+    ## Test Credentials class                                                  ##
+    #############################################################################
+    def test_password(self):
+        '''
+
+        :return:
+        '''
+        pass
+    def test_display_credentials(self):
+        '''
+
+
+        :return:
+        '''
+        pass
+
+    def test_delete_credential(self):
+        '''
+
+        :return:
+        '''
+        pass
+
+    def test_save_credential(self):
+        '''
+
+        :return:
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
