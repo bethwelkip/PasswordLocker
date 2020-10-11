f9c8f7e23be2a292fba8e0b38bff59e6273600f3
Bethwelkip <bkiplimo@princeton.edu>
2020-10-11 14:37:32 -0400

    add methods to delete credentials

--- credentials.py
+++ credentials.py
@@ -1,10 +1,14 @@
 
+import secrets
+
 
 class Credential:
     credentials_list  = []
-    def __init__(self):
-        self.username = self.username()
-        self.password = self.password()
+
+    def __init__(self, name, username, password):
+        self.username = username
+        self.password = password
+        self.name = name
 
     def password(self):
         while True:
@@ -15,23 +19,42 @@ class Credential:
                 password = input()
                 return password
             elif opt == "n":
-                return f"hdhsj{opt}ihsyrwgka"
+                passw = secrets.token_urlsafe()
+                print("Your system generated password is:  ", passw)
+                # lenn = int(input("what's the length of the password you want me to generate? eg  "))
+                # return passw[:lenn]
+                return passw
             else:
-                print("You did not make the right choice")
+                print("You did not make an acceptable choice")
+
     def username(self):
         print("please enter your credential user name")
         user = input()
         return user
 
+    @classmethod
     def display_credentials(self):
-        for cred in Credential.credentials_list:
-            print("/n")
-            print(f"For {cred} your username is: {cred.username} and your password is: {cred.password}")
+        if Credential.credentials_list:
+            for cred in Credential.credentials_list:
+                print("/n")
+                print(f"For {cred.name} your username is: {cred.username}")  # and your password is: {cred.password}
+        else:
+            "You have no credentials yet"
 
-    def delete_credential(self):
-        pass
+    @classmethod
+    def delete_credential(self, name):
+        if Credential.credentials_list:
+            for cred in Credential.credentials_list:
+                if cred.name == name:
+                    Credential.credentials_list.remove(cred)
+                    print(f"The credential {cred.name} has been deleted")
+                    return
+            print("No such a credential exists")
+        else:
+            print("No credentials saved yet. Please save before deleting")
+
     def save_credential(self):
-        pass
+        Credential.credentials_list.append(self)
 
 
 if __name__ == '__main__':
