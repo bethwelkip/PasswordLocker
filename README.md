f66a25d1cc1d1db565e32e900d7ed0139c1172f4
Bethwelkip <bkiplimo@princeton.edu>
2020-10-12 18:32:11 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
+# PasswordLocker
+
+## Description
+Allows the user to create a user account and create their credentials and save them
+
+Topics
+Resources
+
+## Setup/Installation Requirements
+This codebase is standalone and does not require any other code.The code must also be run locally or in a virtual
+environment.
+
+* Open github and clone the folder
+*Run locally
+* click "Enter" on Windows or "Return" on Mac OSX
+* The website should then load.
+
+***********
+
+## Technologies Used
+The project uses Python
+
+## Known Bugs
+There are no known bugs at the moment.If any is discovered, please don't hesitate to contact me.
+
+## Support and contact details
+Please reach out to me at bkiplimo@princeton.edu if you have any questions
+or concerns.
+
+### License
+This extension is licensed under the [MIT License](https://github.com/bethwelkip/GithubSearch/blob/master/LICENSE).
+Copyright (c) {2020} **{Bethwel Kiplimo}**
+
 
--- credentials.py
+++ credentials.py
@@ -3,17 +3,38 @@ import secrets
 
 
 class Credential:
-    credentials_list  = []
+    '''
+    Generates new credentials
+    '''
+    credentials_list = []
 
     def __init__(self, name, username, password):
+        '''
+        Defines properties of the credentials
+
+        :param name:
+        :param username:
+        :param password:
+        '''
         self.username = username
         self.password = password
         self.name = name
 
-    def password(self):
+    def userInput(self, caller):
+        if caller == "password":
+            opt = input("press y to create password, or press n for me to generate a password for you")
+            return opt
+
+    def passworded(self):
+
+        '''
+        Allows user to create password or alternatively generates one for the user
+
+        :return:
+        '''
+
         while True:
-            print("press y to create password, or press n for me to generate a password for you")
-            opt = input()
+            opt = self.userInput("password")
             if opt == "y":
                 print("please input your preferred password")
                 password = input()
@@ -26,9 +47,16 @@ class Credential:
                 return passw
             else:
                 print("You did not make an acceptable choice")
+                return
 
     @classmethod
-    def display_credentials(self):
+    def display_credentials(cls):
+        '''
+        Loops through the credentials and print their properties: password and username
+
+        :return:
+        '''
+
         if len(Credential.credentials_list) > 0:
             for cred in Credential.credentials_list:
                 print("/n")
@@ -37,7 +65,14 @@ class Credential:
             print("You have no credentials yet")
 
     @classmethod
-    def delete_credential(self, name):
+    def delete_credential(cls, name):
+        '''
+        delete the specified credential
+
+
+        :param name:
+        :return:
+        '''
         if Credential.credentials_list:
             for cred in Credential.credentials_list:
                 if cred.name == name:
@@ -49,10 +84,21 @@ class Credential:
             print("No credentials saved yet. Please save before deleting")
 
     def save_credential(self):
+        '''
+        saves the credential object in the class list of instance objects
+
+
+        :return:
+        '''
+
         Credential.credentials_list.append(self)
 
 
 if __name__ == '__main__':
-    pass
+    cew = Credential("hu", "hi", "kj")
+    print(cew.passworded())
+
+
+
 
 
