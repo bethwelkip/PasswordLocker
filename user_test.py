04bf230338240dd48af41d2e9de5ba487da6bce4
Bethwelkip <bkiplimo@princeton.edu>
2020-10-13 07:58:52 -0400

    add methods to test the credentials class

--- user_test.py
+++ user_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import builtins
 import unittest
 from user import User
 from credentials import Credential
+from unittest.mock import patch
 
 class ContactTest(unittest.TestCase):
     '''
@@ -19,6 +20,7 @@ class ContactTest(unittest.TestCase):
         :return:
         '''
         self.user_one = User("Bethu", "Kip")
+        self.cred_one = Credential("twitter", "bethu", 'kip')
 
     def tearDown(self):
         '''
@@ -26,7 +28,8 @@ class ContactTest(unittest.TestCase):
 
         :return:
         '''
-        pass
+        Credential.credentials_list = []
+        User.users = []
 
     def test_init(self):
         '''
@@ -34,33 +37,12 @@ class ContactTest(unittest.TestCase):
 
          :return:
         '''
-        pass
+        self.assertEqual(self.user_one.password, "Kip")
+        self.assertEqual(self.user_one.username, "Bethu")
+        self.assertEqual(self.cred_one.name, "twitter")
+        self.assertEqual(self.cred_one.password, "kip")
+        self.assertEqual(self.cred_one.username, "bethu")
 
-    def test_save_credentials(self):
-        '''
-
-
-        :return:
-        '''
-        pass
-
-    def test_display_creds(self):
-        '''
-
-
-        :return:
-
-        '''
-        pass
-
-    def test_delete_credential(self):
-        '''
-
-
-        :return:
-
-        '''
-        pass
 
     def test_create_credential(self):
         '''
@@ -69,7 +51,12 @@ class ContactTest(unittest.TestCase):
         :return:
 
         '''
-        pass
+
+        input_one = builtins.input("beth")
+        input_two = builtins.input("beth")
+        input_three = builtins.input("y")
+        input_three = builtins.input("beth")
+        user_input = ["beth","beth", "y", "beth"]
 
     def test_add_credential(self):
         '''
@@ -110,7 +97,7 @@ class ContactTest(unittest.TestCase):
     #############################################################################
     ## Test Credentials class                                                  ##
     #############################################################################
-    def test_password(self):
+    def test_passworded(self):
         '''
 
         :return:
@@ -121,23 +108,40 @@ class ContactTest(unittest.TestCase):
 
 
         :return:
+
         '''
-        pass
+        printed = f"\n For {self.cred_one.name} your username is: { self.cred_one.username}  and your password is: {self.cred_one.password}"
+        Credential.credentials_list.append(self.cred_one)
+        self.assertEqual(Credential.display_credentials(), builtins.print(printed))
 
     def test_delete_credential(self):
         '''
 
         :return:
         '''
-        pass
+        Credential.credentials_list.append(self.cred_one)
+        Credential.delete_credential(self.cred_one.name)
+        self.assertEqual(len(Credential.credentials_list), 0)  # should fail
+        # not working as desired
 
     def test_save_credential(self):
+
         '''
+
 
         :return:
         '''
+        Credential.credentials_list.append(self.cred_one)
+        User.users.append(self.user_one)
+        self.assertEqual(len(Credential.credentials_list), 1)
+        self.assertEqual(len(User.users), 1)
+    def test_userInput(self):
+        '''
+        tests the userInput classes
+        :return:
+        '''
+        # method only incorporates IO dependencies which are not generally tested so no tests here.
         pass
-
 
 if __name__ == '__main__':
     unittest.main()
